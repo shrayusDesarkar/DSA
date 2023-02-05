aa365c076c2a5777f2208d34bb3802a3fc371072
Shrayus Desarkar <shrayus.desarkar@gmail.com>
2023-02-05 22:35:43 +0530

    first class

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class first {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.next());
+    }    
+}
